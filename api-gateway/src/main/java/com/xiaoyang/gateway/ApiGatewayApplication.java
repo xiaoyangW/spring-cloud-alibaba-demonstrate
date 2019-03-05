7f5563de6c00d9cc0958ea19a55946a13e2b7cad
wenxiaoyang <xiaoyang.wem@arvato.com>
2019-03-05 14:22:02 +0800

    add api-gateway project

---
+++ api-gateway/src/main/java/com/xiaoyang/gateway/ApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.xiaoyang.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Xiaoyang Wen
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
